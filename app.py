4f4e583c2a13cffff5584a0158cdd5cdcbc4a03b
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2021-10-31 16:09:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 from joblib import load
+from string import digits
 from sklearn.preprocessing import LabelEncoder
 from flask import Flask, render_template, request
 
@@ -18,8 +19,20 @@ def home():
 
 @app.route('/get_data', methods=['POST'])
 def get_data():
-    message = float([x for x in request.form.values()][0])
+    message = [x for x in request.form.values()][0]
     
+    if message[0] == "[":
+        if message[-1] == "]":
+            data = message.split(",")
+            if len(data) != 8:
+                return "Data must consist of 8 features"
+            else:
+                data = [float(''.join(c for c in element if (c in digits or c=='.'))) for element in data]
+                model = load("model.joblib")
+                data = np.array(data).reshape(1, -1)
+                prediction = model.predict(data)[0]
+                return render_template("home.html", pred=f"Food supply will be {prediction} (kcal/capita/day)", data_num='1/8')
+
     f = open("data.txt", "a+")
     f.write(str(message)+'\n')
     f.close()
