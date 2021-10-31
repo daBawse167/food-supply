9d529d0ca152e4ad9a7aa67f958648ae6f9d5fc6
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2021-10-31 14:58:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ n_features = 8
 
 @app.route('/')
 def home():
+    f = open("data.txt", "r+")
+    f.truncate(0)
+    f.close()
     return render_template("home.html", pred='1/8')
 
 @app.route('/get_data', methods=['POST'])
